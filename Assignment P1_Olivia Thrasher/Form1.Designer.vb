d380bfdcfff16d68e90e3e7bda9aae9169a12119
Kalic <Kalic@FLOOF>
2024-02-05 23:20:57 -0500

    Add project files.

---
+++ Assignment P1_Olivia Thrasher/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        ButtonExit = New Button()
+        Text1Un = New TextBox()
+        Text2De = New TextBox()
+        Text3Tr = New TextBox()
+        Text4Qu = New TextBox()
+        Text5Ci = New TextBox()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = SystemColors.Control
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(140, 36)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(569, 27)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "Do you know the French words for the numbers 1 through 5?"
+        TextBox1.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = SystemColors.Control
+        TextBox2.BorderStyle = BorderStyle.None
+        TextBox2.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TextBox2.Location = New Point(177, 69)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(494, 27)
+        TextBox2.TabIndex = 1
+        TextBox2.Text = "Click the buttons below to see them."
+        TextBox2.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ButtonHighlight
+        Button1.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(177, 137)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "1"
+        Button1.TextAlign = ContentAlignment.TopCenter
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.ButtonHighlight
+        Button2.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(277, 137)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 3
+        Button2.Text = "2"
+        Button2.TextAlign = ContentAlignment.TopCenter
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.ButtonHighlight
+        Button3.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(377, 137)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 4
+        Button3.Text = "3"
+        Button3.TextAlign = ContentAlignment.TopCenter
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = SystemColors.ButtonHighlight
+        Button4.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(477, 137)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 5
+        Button4.Text = "4"
+        Button4.TextAlign = ContentAlignment.TopCenter
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = SystemColors.ButtonHighlight
+        Button5.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button5.Location = New Point(577, 137)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(94, 29)
+        Button5.TabIndex = 6
+        Button5.Text = "5"
+        Button5.TextAlign = ContentAlignment.TopCenter
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonExit
+        ' 
+        ButtonExit.BackColor = SystemColors.ButtonHighlight
+        ButtonExit.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonExit.Location = New Point(377, 306)
+        ButtonExit.Name = "ButtonExit"
+        ButtonExit.Size = New Size(94, 32)
+        ButtonExit.TabIndex = 7
+        ButtonExit.Text = "Exit"
+        ButtonExit.TextAlign = ContentAlignment.TopCenter
+        ButtonExit.UseVisualStyleBackColor = False
+        ' 
+        ' Text1Un
+        ' 
+        Text1Un.BackColor = SystemColors.ActiveCaption
+        Text1Un.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Text1Un.Location = New Point(377, 218)
+        Text1Un.Name = "Text1Un"
+        Text1Un.Size = New Size(94, 32)
+        Text1Un.TabIndex = 8
+        Text1Un.Text = "Un"
+        Text1Un.TextAlign = HorizontalAlignment.Center
+        Text1Un.Visible = False
+        ' 
+        ' Text2De
+        ' 
+        Text2De.BackColor = SystemColors.ActiveCaption
+        Text2De.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Text2De.Location = New Point(377, 218)
+        Text2De.Name = "Text2De"
+        Text2De.Size = New Size(94, 32)
+        Text2De.TabIndex = 9
+        Text2De.Text = "Deux"
+        Text2De.TextAlign = HorizontalAlignment.Center
+        Text2De.Visible = False
+        ' 
+        ' Text3Tr
+        ' 
+        Text3Tr.BackColor = SystemColors.ActiveCaption
+        Text3Tr.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Text3Tr.Location = New Point(377, 218)
+        Text3Tr.Name = "Text3Tr"
+        Text3Tr.Size = New Size(94, 32)
+        Text3Tr.TabIndex = 10
+        Text3Tr.Text = "Trois"
+        Text3Tr.TextAlign = HorizontalAlignment.Center
+        Text3Tr.Visible = False
+        ' 
+        ' Text4Qu
+        ' 
+        Text4Qu.BackColor = SystemColors.ActiveCaption
+        Text4Qu.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Text4Qu.Location = New Point(377, 218)
+        Text4Qu.Name = "Text4Qu"
+        Text4Qu.Size = New Size(94, 32)
+        Text4Qu.TabIndex = 11
+        Text4Qu.Text = "Quatre"
+        Text4Qu.TextAlign = HorizontalAlignment.Center
+        Text4Qu.Visible = False
+        ' 
+        ' Text5Ci
+        ' 
+        Text5Ci.BackColor = SystemColors.ActiveCaption
+        Text5Ci.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Text5Ci.Location = New Point(377, 218)
+        Text5Ci.Name = "Text5Ci"
+        Text5Ci.Size = New Size(94, 32)
+        Text5Ci.TabIndex = 12
+        Text5Ci.Text = "Cinq"
+        Text5Ci.TextAlign = HorizontalAlignment.Center
+        Text5Ci.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Text4Qu)
+        Controls.Add(Text3Tr)
+        Controls.Add(Text2De)
+        Controls.Add(Text1Un)
+        Controls.Add(ButtonExit)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Text5Ci)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "French Numbers"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ButtonExit As Button
+    Friend WithEvents Text1Un As TextBox
+    Friend WithEvents Text2De As TextBox
+    Friend WithEvents Text3Tr As TextBox
+    Friend WithEvents Text4Qu As TextBox
+    Friend WithEvents Text5Ci As TextBox
+
+End Class
---
+++ Assignment P1_Olivia Thrasher/Form1.vb
@@ -0,0 +1,41 @@
+﻿'Olivia Thrasher
+'Assignment P1
+'GUI Development
+
+Public Class Form1
+    'Pressing Button 1 will reveal Un and move to the front
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Text1Un.Visible = True
+        Text1Un.BringToFront()
+    End Sub
+
+    'Pressing Button 2 will reveal De and move to the front 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Text2De.Visible = True
+        Text2De.BringToFront()
+    End Sub
+
+    'Pressing Button 3 will reveal Tr and move to the front
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Text3Tr.Visible = True
+        Text3Tr.BringToFront()
+    End Sub
+
+    'Pressing Button 4 will reveal Qu and move to the front
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Text4Qu.Visible = True
+        Text4Qu.BringToFront()
+    End Sub
+
+    'Pressing Button 5 will reveal Ci and move to the front
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Text5Ci.Visible = True
+        Text5Ci.BringToFront()
+    End Sub
+
+    'Pressing Exit button will close the program
+    Private Sub ButtonExit_Click(sender As Object, e As EventArgs) Handles ButtonExit.Click
+        Close()
+    End Sub
+
+End Class
